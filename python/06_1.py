2f5666e1ae3fb5a2de655d332422df04ebe9754b
adventElf <guardian.song3566@fastmail.com>
2022-12-06 00:48:50 -0500

    Day 6

---
+++ python/06_1.py
@@ -0,0 +1,17 @@
+def process_input(input="../inputs/06_1.txt"):
+    stream = open(input, "r").read()
+    span = 4
+    start = 0
+    end = span
+    searching = True
+    while searching:
+        window = set(stream[start:end])
+        if len(window) < span:
+            start += 1
+            end += 1
+        elif len(window) == span:
+            print(window)
+            return end
+
+
+process_input()
